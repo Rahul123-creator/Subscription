4402681987ec4f51bc78336c38e3f519c0ab71a6
Rahul Paul <rahulpaulkv24@gmail.com>
2023-08-20 13:03:24 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SubscriptionForm from './SubscriptionForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SubscriptionForm />
+    </div>
+  );
+}
+
+export default App;
